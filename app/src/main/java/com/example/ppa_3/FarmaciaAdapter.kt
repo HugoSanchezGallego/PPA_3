313fd4893d5bac8d7200882880db3c725f57ebc5
HugoSanchezGallego <hsancgal@myuax.com>
2024-12-03 23:32:13 +0100

    Funciona firebase + icono de farmacia

--- app/src/main/java/com/example/ppa_3/FarmaciaAdapter.kt
+++ app/src/main/java/com/example/ppa_3/FarmaciaAdapter.kt
@@ -3,6 +3,7 @@ package com.example.ppa_3
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -10,10 +11,11 @@ class FarmaciaAdapter(private val farmacias: List<Farmacia>) :
     RecyclerView.Adapter<FarmaciaAdapter.FarmaciaViewHolder>() {
 
     class FarmaciaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title: TextView = itemView.findViewById(R.id.title)
-        val telefono: TextView = itemView.findViewById(R.id.telefono)
-        val coordinates: TextView = itemView.findViewById(R.id.coordinates)
-    }
+    val title: TextView = itemView.findViewById(R.id.title)
+    val telefono: TextView = itemView.findViewById(R.id.telefono)
+    val coordinates: TextView = itemView.findViewById(R.id.coordinates)
+    val image: ImageView = itemView.findViewById(R.id.farmacia_image)
+}
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FarmaciaViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -22,11 +24,12 @@ class FarmaciaAdapter(private val farmacias: List<Farmacia>) :
     }
 
     override fun onBindViewHolder(holder: FarmaciaViewHolder, position: Int) {
-        val farmacia = farmacias[position]
-        holder.title.text = farmacia.title
-        holder.telefono.text = farmacia.telefono
-        holder.coordinates.text = farmacia.coordinates.joinToString(", ")
-    }
+    val farmacia = farmacias[position]
+    holder.title.text = farmacia.title
+    holder.telefono.text = farmacia.telefono
+    holder.coordinates.text = farmacia.coordinates.joinToString(", ")
+    holder.image.setImageResource(R.drawable.farmacia_foto)
+}
 
     override fun getItemCount() = farmacias.size
 }
